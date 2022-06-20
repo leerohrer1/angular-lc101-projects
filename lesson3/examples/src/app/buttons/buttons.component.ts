1ef431dcec0658998d15e24f19f29d0b74b14d15
leerohrer1 <leerohrer1@gmail.com>
2022-06-20 13:36:00 -0500

    Finished lesson 3 practice

--- lesson3/examples/src/app/buttons/buttons.component.ts
+++ lesson3/examples/src/app/buttons/buttons.component.ts
@@ -1,16 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-   selector: 'buttons',
-   templateUrl: './buttons.component.html',
-   styleUrls: ['./buttons.component.css']
+  selector: "buttons",
+  templateUrl: "./buttons.component.html",
+  styleUrls: ["./buttons.component.css"],
 })
 export class ButtonsComponent implements OnInit {
-   buttonHeading: string = "Buttons"
-   inactive: boolean = false;
+  buttonHeading: string = "Buttons";
+  buttonState: boolean[] = [true, true, true];
+  active: string = 'true';
+  location: string = "center";
 
-   constructor() { }
+  jokeHeading: string = "Do you like buttons?";
+  jokeButtonAlignment: string[] = ["right", "left", "center"];
 
-   ngOnInit() { }
+  constructor() {}
+
+  ngOnInit() {}
+
+  reactivateButtons() {
+    return (this.buttonState = this.buttonState.map((e) => (e = true)));
+  }
 
 }
